f8bda12dfe035f909b79c6cdef72897f3f16cb74
Mark Richards <mark@black13.debian.markoffline.com>
2019-08-21 14:08:53 +0100

    improvedsync

--- localsystemd/dockerdevsync.sh
+++ localsystemd/dockerdevsync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-rsync -avz --no-perms --no-owner --no-group --delete /home/mark/syncthing/local/ vagrant@dockerdev.local:/home/vagrant/syncthing/local
+rsync -avz --no-perms --no-owner --no-group --delete /home/mark/syncthing/local/ vagrant@dockerdev.local:/home/vagrant/syncthing/local/
+rsync -avz --exclude=local --no-perms --no-owner --no-group --delete  vagrant@dockerdev.local:/home/vagrant/syncthing/ /home/mark/syncthing/
 
 
