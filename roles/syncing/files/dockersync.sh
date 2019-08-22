dd131fb8afc843ff6b4266b8172f16b3058af1fe
Mark Richards <mark@black13.debian.markoffline.com>
2019-08-22 17:47:22 +0100

    Ignore .stfolder

--- roles/syncing/files/dockersync.sh
+++ roles/syncing/files/dockersync.sh
@@ -6,5 +6,5 @@ docker cp -a compose_syncthing_1:/opt/syncthing/ - > /tmp/syncthing/syncthing.ta
 sleep 1
 tar -xf /tmp/syncthing/syncthing.tar -C /tmp/syncthing
 rsync -avz --exclude=local --no-perms --no-owner --no-group --delete /tmp/syncthing/syncthing/ /home/vagrant/syncthing/
-docker run --rm  -v syncthing:/opt/syncthing -v /home/vagrant/syncthing:/opt/tosync --user=syncthing compose_syncthing rsync -avz --no-perms --no-owner --no-group --delete /opt/tosync/local/ /opt/syncthing/local/
+docker run --rm  -v syncthing:/opt/syncthing -v /home/vagrant/syncthing:/opt/tosync --user=syncthing compose_syncthing rsync -avz --exclude=.stfolder --exclude=.stignore --no-perms --no-owner --no-group --delete /opt/tosync/local/ /opt/syncthing/local/
 
