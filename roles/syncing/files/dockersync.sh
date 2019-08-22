318be873164a21e81d8a7e6f1b132691550c3b1a
Mark Richards <mark@black13.debian.markoffline.com>
2019-08-22 17:40:06 +0100

    fixing local path

--- roles/syncing/files/dockersync.sh
+++ roles/syncing/files/dockersync.sh
@@ -3,7 +3,8 @@ set -xe
 rm -rf /tmp/syncthing
 mkdir /tmp/syncthing
 docker cp -a compose_syncthing_1:/opt/syncthing/ - > /tmp/syncthing/syncthing.tar
+sleep 1
 tar -xf /tmp/syncthing/syncthing.tar -C /tmp/syncthing
 rsync -avz --exclude=local --no-perms --no-owner --no-group --delete /tmp/syncthing/syncthing/ /home/vagrant/syncthing/
-docker run --rm  -v syncthing:/opt/syncthing -v /home/vagrant/syncthing:/opt/tosync --user=syncthing compose_syncthing rsync -avz --no-perms --no-owner --no-group --delete /opt/tosync/local /opt/syncthing/local
+docker run --rm  -v syncthing:/opt/syncthing -v /home/vagrant/syncthing:/opt/tosync --user=syncthing compose_syncthing rsync -avz --no-perms --no-owner --no-group --delete /opt/tosync/local/ /opt/syncthing/local/
 
