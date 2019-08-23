1e5d9ec3240442a75fd5f38473225a46150db878
Mark Richards <mark@black13.debian.markoffline.com>
2019-08-23 17:27:57 +0100

    Using shared memory instead of /tmp

--- roles/syncing/files/dockersync.sh
+++ roles/syncing/files/dockersync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -xe
-rm -rf /tmp/syncthing
-mkdir /tmp/syncthing
-docker cp -a compose_syncthing_1:/opt/syncthing/ - > /tmp/syncthing/syncthing.tar
+rm -rf /dev/shm/syncthing
+mkdir /dev/shm/syncthing
+docker cp -a compose_syncthing_1:/opt/syncthing/ - > /dev/shm/syncthing/syncthing.tar
 sleep 1
-tar -xf /tmp/syncthing/syncthing.tar -C /tmp/syncthing
-rsync -avz --exclude=local --no-perms --no-owner --no-group --delete /tmp/syncthing/syncthing/ /home/vagrant/syncthing/
+tar -xf /dev/shm/syncthing/syncthing.tar -C /dev/shm/syncthing
+rsync -avz --exclude=local --no-perms --no-owner --no-group --delete /dev/shm/syncthing/syncthing/ /home/vagrant/syncthing/
 docker run --rm  -v syncthing:/opt/syncthing -v /home/vagrant/syncthing:/opt/tosync --user=syncthing compose_syncthing rsync -avz --exclude=.stfolder --exclude=.stignore --no-perms --no-owner --no-group --delete /opt/tosync/local/ /opt/syncthing/local/
 
