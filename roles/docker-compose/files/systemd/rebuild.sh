b3cd17726bdbcf572cd784b0c62b1155744b1834
Mark Richards <mark.alan.richards@gmail.com>
2020-03-28 09:56:57 +0000

    improving

--- roles/docker-compose/files/systemd/rebuild.sh
+++ roles/docker-compose/files/systemd/rebuild.sh
@@ -3,5 +3,7 @@ set -xe
 
 source /opt/docker-compose/bin/activate
 cd /opt/docker-compose/compose
+docker-compose down
 docker-compose build --pull --no-cache
-
+docker-compose up --force-recreate -d
+docker system prune -a -f
